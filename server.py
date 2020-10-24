0c9c8ea487bf51e3aa446d46fdd619572acae980
SagarMD12194 <sagarmd888@gmail.com>
2020-10-24 16:35:11 +0530

    First commit to Flask app

---
+++ server.py
@@ -0,0 +1,32 @@
+from builtins import print
+from flask import Flask, render_template, redirect, request
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def index(page_name=None):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit(page_name=None):
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+
+        with open("database.csv", "a", newline='') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow([email, subject, message])
+
+    else:
+        print("This is not post request")
+    return redirect("/thank_you.html")
